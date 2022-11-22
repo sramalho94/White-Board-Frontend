c61e883e22355f3443468d9b1d1a2b83ab91b783
Stephan D Ramalho <sramalho@fordham.edu>
2022-11-22 11:03:48 -0500

    finished add courseform

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,7 +7,7 @@ let authenticatedOptions
 
 if (user) {
     authenticatedOptions = (
-        <div>
+        <div className="nav">
             <h3>Welcome {user.email}</h3>
             <Link to='/'>Home</Link>
             <Link onClick={handleLogout} to='/'>Logout?</Link>
@@ -18,7 +18,7 @@ if (user) {
 
 
 const publicOptions = (
-    <nav>
+    <nav className="nav">
 <Link to='/'>Home</Link>
 <Link to='/register'>Register</Link>
 <Link to='/login'>Login</Link>
--- src/pages/AddCourseForm.js
+++ src/pages/AddCourseForm.js
@@ -15,12 +15,40 @@ const AddCourseForm = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault()
-    const newCourse = await axios.post('http://localhost:3001/api/course/create')
-      name: formValues.name,
-      email: formValues.email,
-      password: formValues.password
-  
-    return <div>AddCourseForm</div>
+    const newCourse = await axios.post(
+      'http://localhost:3001/api/course/create',
+      {
+        name: formValues.name
+      }
+    )
+    console.log('created course')
   }
+  return (
+    <div className="signin col">
+      <div className="card-overlay centered">
+        <form className="col" onSubmit={handleSubmit}>
+          <h1>Hello</h1>
+          <p>Add a New Course Below</p>
+          <div className="input-wrapper">
+            <input
+              className="name"
+              onChange={handleChange}
+              name="name"
+              type="text"
+              placeholder="Name"
+              value={formValues.name}
+              required
+            />
+          </div>
+          <div className="button1">
+            <button className="regButton" disabled={!formValues.name}>
+              Add Course
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  )
 }
+
 export default AddCourseForm
